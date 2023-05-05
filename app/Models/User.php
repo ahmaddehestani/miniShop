7054d118e75993cbd397ef1a95110c7d84d5254d
AhmadDehestani <s.ahmad.dehestani@gmail.com>
2023-05-05 10:25:16 +0330

    add: city model & migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'address',
+        'phone',
+        'province_id',
+        'city_id'
     ];
 
     /**
---
+++ app/Models/city.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/*
+ * @property foreignId province_id
+ * @property string name
+ */
+
+class city extends Model
+{
+    use HasFactory;
+    protected $table = 'cities';
+
+    protected $fillable = [
+
+        'province_id',
+        'name',
+
+    ];
+}
